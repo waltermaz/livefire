60a72205df0b7ddd716ffd43f4de34ad9269f4c0
waltermaz <happy8D@gmail.com>
2021-02-18 09:07:12 -0800

    first commit to livefire.git

---
+++ hello-world.go
@@ -0,0 +1,10 @@
+# add a new comment
+# add a second comment
+# file:  hello-world.go
+# description:  my very first go program
+
+package main
+import "fmt"
+func main() {
+    fmt.Println("hello world... glad to meet you!")
+}
